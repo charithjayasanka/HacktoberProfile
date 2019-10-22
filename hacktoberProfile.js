94576f68ed0c71644eef0790cb09c88467b960cd
Charith Jayasanka <charithj@zone24x7.com>
2019-10-22 17:09:07 +0530

    name added

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+   {
+    name : "Charith Jayasanka",
+    place : "Kurunegala, Sri Lanka",
+    github : "charithjayasanka"
   }
 ];
